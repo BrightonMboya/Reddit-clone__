a2d4c8e37c1fe26c0a358b091d3d98eebb08f54b
BrightonMboya <b.mboya@alustudent.com>
2022-04-10 13:12:52 +0200

    subreddit front-end in mobile version

---
+++ reddit-clone/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PostCards from './components/PostCards';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="">
+      <NavBar/>
+      <PostCards/>
+    </div>
+  );
+}
+
+export default App;
